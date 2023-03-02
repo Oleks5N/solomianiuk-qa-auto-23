c21018a19fcc6857dfb3609736dbf06602bdb3d6
Oleksandr Solomianiuk <oleksandr.solomianiuk@gmail.com>
2023-03-02 16:38:19 +0200

    New http test added

--- tests/api/test_http.py
+++ tests/api/test_http.py
@@ -14,13 +14,22 @@ def test_second_request():
     body = r.json()
     headers = r.headers
 
+    assert body['login'] == 'defunkt'
     assert body['name'] == 'Chris Wanstrath'
     assert r.status_code == 200
     assert headers['Server'] == 'GitHub.com'
 
 
 @pytest.mark.http
+def test_third_request():
+    r = requests.get('https://api.github.com/versions')
+    print(f"Response is {r.text}")
+
+    assert r.status_code == 200
+
+
+@pytest.mark.http
 def test_status_code_request():
-    r = requests.get('https://api.github.com/users/sergii_butenko')
+    r = requests.get('https://api.github.com/users/oleksandr_solomianiuk')
 
     assert r.status_code == 404
